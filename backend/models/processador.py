87af613ce3876f54007e327c6d74eb7132831154
Arthur <arthurfr.backup@gmail.com>
2026-08-14 19:28:29 -0300

    criacao de tabelas

---
+++ backend/models/processador.py
@@ -0,0 +1,19 @@
+from . import db
+from . import Peca
+
+class Processador(Peca):
+    __tablename__ = "processador"
+
+    id = db.Column(db.Integer(), primary_key=True)
+    marca = db.Column(db.String(100), nullable=False)
+    modelo = db.Column(db.String(100), nullable=False)
+    preco = db.Column(db.Numeric(10, 2), nullable=False)
+    soquete = db.Column(db.String(100), nullable=False)
+    nucleo = db.Column(db.Integer(), nullable=False)
+    thread = db.Column(db.Integer(), nullable=False)
+    clockbase = db.Column(db.Numeric(), nullable=False)
+    clockmax = db.Column(db.Numeric(), nullable=False)
+    videointegrado = db.Column(db.Boolean(), default=False)
+    modelo_videointegrado = db.Column(db.String(100))
+    cooler = db.Column(db.Boolean(), default=False)
+    modelo_cooler = db.Column(db.String(100))
