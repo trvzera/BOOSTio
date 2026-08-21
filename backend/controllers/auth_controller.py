c5c29cd2e337234bb51e721c136a850fd576c7dd
Gustavo Azevedo <gustavoazevedo.code@gmail.com>
2026-08-21 15:25:15 -0300

    login google finalizado

--- backend/controllers/auth_controller.py
+++ backend/controllers/auth_controller.py
@@ -1,10 +1,11 @@
-from flask import Blueprint,request,jsonify
+from flask import Blueprint,request,jsonify,redirect,url_for
 from flask_login import LoginManager, UserMixin, login_user,logout_user, login_required, current_user
 from models import db,Usuario
 from sqlalchemy.exc import SQLAlchemyError
 from models import lm
-
+from configs import oauth
 from services.usuario_service.entrar_usuario_service import EntrarUsuarioService
+from services.usuario_service.entrar_google_usuario_service import EntrarGoogleService
 
 auth_bp = Blueprint("auth",__name__,url_prefix='/auth')
 
@@ -49,3 +50,23 @@ def teste_logado():
                 "auth":False,
                 "usuario":None
             }),200
+
+
+@auth_bp.get("/google/redirecionar")
+def redirecionar_google():
+    redirect_uri = url_for('auth.callback_google', _external=True)
+    return oauth.google.authorize_redirect(redirect_uri)
+
+
+@auth_bp.get("/google/entrar")
+def callback_google():
+    try:
+        token = oauth.google.authorize_access_token()
+        dados_usuario = token.get('userinfo')
+
+        usuario = EntrarGoogleService().executar(dados_usuario)
+        login_user(usuario)
+
+        return redirect('http://localhost:5500/pages/index.html') #Redirect obrigatorio, o google espera um retorno desse tipo
+    except ValueError as erro:
+        return redirect(f'http://localhost:5500/pages/login.html?erro={str(erro)}')
