ab02e1aade4059d8873a643338e8500bb9ec6472
Gustavo Azevedo <gustavoAzevedo.code@gmail.com>
2026-08-22 15:56:59 -0300

    bug de fazer login apos ter criado a conta com google resolvido

--- backend/services/usuario_service/entrar_usuario_service.py
+++ backend/services/usuario_service/entrar_usuario_service.py
@@ -7,6 +7,9 @@ class EntrarUsuarioService:
     validacao_campos(dados,["email","senha"])
 
     usuario = Usuario.buscar_por_email(dados["email"].lower())
+
+    if usuario and usuario.senha is None:
+      raise ValueError(f"Experimente fazer login com o Google")
     
     if not usuario or not check_password_hash(usuario.senha,dados["senha"]):
       raise ValueError(f"Senha ou email inválidos")
